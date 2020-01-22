7bd4935c3616b4a6f2f0e2beea36fb4bac1c59e8
Ilija Janchev <i.janchev@gmail.com>
2020-01-22 15:07:05 +0100

    removed redux-logger

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ import ReactDOM from "react-dom";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import "tachyons";
-
 import App from "./containers/App";
 import registerServiceWorker from "./serviceWorker";
 import { requestRobots, searchRobots } from "./reducers";
-
 import "./index.css";
-
-const logger = createLogger();
 
 const rootReducers = combineReducers({ requestRobots, searchRobots });
 
-const store = createStore(
-  rootReducers,
-  applyMiddleware(thunkMiddleware, logger)
-);
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
